6f697bd46aa3aca589222ea103534a8bb05ff2ec
danimv <danimv87@gmail.com>
2023-12-26 11:19:32 +0100

    env deleted

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.BACKEND_URL;
+const url = 'https://s1-w5x5.onrender.com';//process.env.BACKEND_URL;
 const api = axios.create({
   baseURL: url,
   withCredentials: true,
