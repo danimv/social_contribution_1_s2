6f64c08d363a11d9d5184baa2271ce56da864310
danimv <danimv87@gmail.com>
2023-12-26 02:41:54 +0100

    a

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 // axios.defaults.withCredentials = true;
-const url ='https://s1-w5x5.onrender.com';// 'https://s1-w5x5.onrender.com:5000';//'http://192.168.1.23:5000'//
+// const url ='http://192.168.1.23:5000';// 'https://s1-w5x5.onrender.com:5000';//'http://192.168.1.23:5000'//
 
+const url = process.env.BACKEND_URL;
 const api = axios.create({
   baseURL: url, //process.env.REACT_APP_API_URL, // Set the base URL for
     withCredentials: true, 
