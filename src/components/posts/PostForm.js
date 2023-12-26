67664830af6f8cffeb71b17c0260a72499197af1
danimv <danimv87@gmail.com>
2023-12-26 22:06:55 +0100

    postform

--- src/components/posts/PostForm.js
+++ src/components/posts/PostForm.js
@@ -1,91 +1,111 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
-import { addPost } from "../../actions/postActions";
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addPost } from '../../actions/postActions';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import { api } from '../../actions/api';
 
-class PostForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
-      errors: {}
-    };
+const PostForm = ({ auth, errors, addPost }) => {
+  const [newData, setData] = useState(null);
+  const [text, setText] = useState('');
+  const [selectedType, setSelectedType] = useState('');
+  const [quantitat, setQuantitat] = useState('');
+  const [unitat, setUnitat] = useState('');
 
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+  api.get('/contrib/new', setData);
 
-  componentWillReceiveProps(newProps) {
-    if (newProps.errors) {
-      this.setState({
-        errors: newProps.errors
-      });
-    }
-  }
+  useEffect(() => {
+    // You can handle side effects or validations here
+    // For example, updating unitat based on selectedType
+    // setUnitat('Kilometres');
+  }, [selectedType]);
 
-  onSubmit(e) {
+  const handleTypeChange = (e) => {
+    setSelectedType(e.target.value);
+    // You can also set unitat based on the selectedType here
+  };
+
+  const onSubmit = (e) => {
     e.preventDefault();
-
-    const { user } = this.props.auth;
-
+    const { user } = auth;
     const newPost = {
-      text: this.state.text,
-      name: user.name
+      text,
+      name: user.name,
     };
 
-    this.props.addPost(newPost);
-    this.setState({
-      text: ""
-    });
-  }
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-  render() {
-    const { errors } = this.state;
-    return (
-      <div className="post-form mb-3">
-        <div className="card card-info">
-          <div className="card-header bg-info text-white">
-            What's on your mind?
-          </div>
+    addPost(newPost);
+    // Clear form data after submission
+    setText('');
+  };
 
-          <div className="card-body">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <TextAreaFieldGroup
-                  placeholder="Create Post"
-                  name="text"
-                  value={this.state.text}
-                  onChange={this.onChange}
-                  error={errors.text}
-                />
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        <h1 style={{ textAlign: 'center' }}>Nova acció</h1>
+
+        <div style={{ width: '50%', margin: 'auto auto', textAlign: 'center' }}>
+          <form className="p-2 bg-light border rounded" onSubmit={onSubmit}>
+            <div className="row g-2">
+              <div className="col-md">
+                <div className="form-floating mb-3">
+                <select
+                    className="form-control custom-select-lg"
+                    id="tipus"
+                    name="tipus"
+                    value={selectedType}
+                    onChange={handleTypeChange}>
+                    <option value="" selected></option>
+                    {newData &&
+                      newData.map((option) => (
+                        <option key={option.name} value={option.name}>
+                          {option.name}
+                        </option>
+                      ))}
+                  </select>
+                  <label htmlFor="tipus" className="form-label">
+                    Tipus d'acció
+                  </label>
+                </div>
               </div>
-
-              <button type="submit" className="btn btn-dark">
-                Post
+              <div className="col-md">
+                <div className="form-floating mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="quantitat"
+                    name="quantitat"
+                    value={quantitat}
+                    onChange={(e) => setQuantitat(e.target.value)}
+                  />
+                  <input type="hidden" className="form-control" id="unitat" name="unitat" value={unitat} />
+                  <label className="form-label" htmlFor="quantitat" id="unitats" name="unitats">
+                    Indica els <span id="unitatLabel">{unitat}</span>
+                  </label>
+                </div>
+              </div>
+            </div>
+            {/* Additional form fields go here */}
+            <div className="form-floating mb-3">
+              <button type="submit" className="btn btn-success">
+                Envia
               </button>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 PostForm.propTypes = {
   auth: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
-  addPost: PropTypes.func.isRequired
+  addPost: PropTypes.func.isRequired,
 };
-const mapStateToProps = state => ({
+
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  errors: state.errors
+  errors: state.errors,
 });
-export default connect(
-  mapStateToProps,
-  { addPost }
-)(PostForm);
+
+export default connect(mapStateToProps, { addPost })(PostForm);
